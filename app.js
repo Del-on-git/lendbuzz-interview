a1fd17137d0269732e3c25e05a13ce941fc67c02
Dean Elizardo <del@delizardo.com>
2023-04-14 11:05:52 -0500

    pipeline end to end test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PG_CONFIG = {
 };
 
 app.get("/", function (req, res) {
-  res.send("Hello, world! Fancy meeting you here ;)");
+  res.send("Hello, world!");
 });
 
 app.get("/db_healthcheck", async function (_, res) {
